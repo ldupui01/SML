c4ef9b99d85f84d539ec336a7b341844bc6d8d96
Guilherme Ribeiro <guilherme.ctr@gmail.com>
2014-02-24 17:11:39 +0000

    first commit

---
+++ SML/src/test/AddInstructionTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddInstructionTest {
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testExecute() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddInstructionStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddInstructionStringIntIntInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testInstruction() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ SML/src/test/MachineTest.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Guilherme
+ *
+ */
+public class MachineTest {
+
+	/**
+	 * Test method for {@link sml.Machine#hashCode()}.
+	 */
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#main(java.lang.String[])}.
+	 */
+	@Test
+	public final void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#execute()}.
+	 */
+	@Test
+	public final void testExecute() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#getLabels()}.
+	 */
+	@Test
+	public final void testGetLabels() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#getProg()}.
+	 */
+	@Test
+	public final void testGetProg() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#getRegisters()}.
+	 */
+	@Test
+	public final void testGetRegisters() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#getPc()}.
+	 */
+	@Test
+	public final void testGetPc() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#setLabels(Labels)}.
+	 */
+	@Test
+	public final void testSetLabels() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#setProg(ArrayList)}.
+	 */
+	@Test
+	public final void testSetProg() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#setRegisters(Registers)}.
+	 */
+	@Test
+	public final void testSetRegisters() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#setPc(int)}.
+	 */
+	@Test
+	public final void testSetPc() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#equals(java.lang.Object)}.
+	 */
+	@Test
+	public final void testEqualsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#canEqual(java.lang.Object)}.
+	 */
+	@Test
+	public final void testCanEqual() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sml.Machine#Machine()}.
+	 */
+	@Test
+	public final void testMachine() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
