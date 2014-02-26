c7cf1f14869682d91d1a7578753611aace2d77ff
Guilherme Ribeiro <guilherme.ctr@gmail.com>
2014-02-26 22:04:20 +0000

    Wrote tests for the composers of linInstruction

--- SML/src/sml/LinInstructionTest.java
+++ SML/src/sml/LinInstructionTest.java
@@ -7,15 +7,17 @@ import org.junit.Test;
 
 public class LinInstructionTest {
 	Instruction lin;
+	String example;
 	@Before
 	public void setUp() throws Exception {
 		lin = new LinInstruction("L1",0,5);
+		example = lin.toString();
 	}
 
 	@Test
 	public final void testToString() {
 		String expected = "L1: lin register 0 value is 5";
-		String input = lin.toString();
+		String input = example;
 		assertEquals("ToString Overriding not working properly or not implemented", expected, input);
 	}
 
@@ -26,12 +28,33 @@ public class LinInstructionTest {
 
 	@Test
 	public final void testLinInstruction() {
-		fail("Not yet implemented"); // TODO
+		String[] expected = new String[3];
+		expected[0] = "L1:";
+		expected[1] = "0";
+		expected[2] = "5";
+		String[] exampleArray = example.split(" ");
+		String[] input = new String[3];
+		input[0] = exampleArray[0];
+		input[1] = exampleArray[3];
+		input[2] = exampleArray[6];
+		
+		assertTrue("LinInstruction is not an instance of instruction", lin instanceof Instruction);
+		assertArrayEquals("label, register and parameter are not being translated properly",expected, input);
 	}
 
 	@Test
 	public final void testInstruction() {
-		fail("Not yet implemented"); // TODO
+		String[] expected = new String[2];
+		expected[0] = "L1:";
+		expected[1] = "lin";
+		String[] exampleArray = example.split(" ");
+		String[] input = new String[2];
+		input[0] = exampleArray[0];
+		input[1] = exampleArray[1];
+	
+		
+		assertTrue("LinInstruction is not an instance of instruction", lin instanceof Instruction);
+		assertArrayEquals("label and opcode are not being translated properly",expected, input);
 	}
 
 }
